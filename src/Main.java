27bbd657c222a7a0f59ec3a94529fe0de226e022
joelmalacas <joelmalacas@gmail.com>
2026-03-20 22:28:32 +0000

    Upload do ficheiro palavras.txt no projeto e implementação e testes do método LerFicheiro e adicionar as palavras no vetor aux

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,71 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+//import Classes; //Import Package Classes dos Respetivos Algoritmos
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HELLO WORLD");
+        long inicio,fim,tempo;
+
+        inicio = System.currentTimeMillis();
+ 
+        //TODO
+        String[] palavras = LerFicheiro("palavras.txt"); //Chama método para contar as palavras para saber o tamanho do vetor Aux
+       
+        fim=System.currentTimeMillis();
+        tempo=fim-inicio;
+    }
+
+    static String[] LerFicheiro(String caminho) {
+
+        File ficheiro=new File(caminho);
+        
+        Scanner leitor=null;
+
+        try {
+            leitor = new Scanner(ficheiro);
+        } catch (FileNotFoundException e) {
+            System.out.println("Ficheiro não encontrado");
+            System.out.println("Erro: " + e); //DEBUG
+        }
+
+        //While ==> Contar as palavras
+        int count = 0;
+        while(leitor.hasNext()){
+            leitor.next();
+            count++;
+        }
+
+        leitor.close();
+
+        //============================
+
+        String[] palavras = new String[count];
+
+        try {
+            leitor = new Scanner(ficheiro);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return new String[0];
+        }
+
+        //While ==> Colocar as palavras no vetor Aux
+        int i = 0;
+        while (leitor.hasNext()) {
+            palavras[i] = leitor.next();
+            i++;
+        }
+
+        leitor.close();
+
+        //Imprimir as palavras para testar o vetor
+        for (int j = 0; j < palavras.length; j++) {
+            System.out.println(palavras[j]);
+        }
+
+        System.out.println("\n\nForam imprimidas " + count + " palavras no vetor aux");
+
+        return palavras;
     }
 }
